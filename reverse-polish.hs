2347ccc3616ab7f7df58b0c751f3197c1b48b8f5
Aditya Joshi <1adityajoshi@gmail.com>
2015-05-30 21:36:16 +0530

    added code for rpn.

--- reverse-polish.hs
+++ reverse-polish.hs
@@ -1,5 +1,6 @@
-import Data.list
-
-solveRPN :: (Num a) => String -> a
-solveRPN expression = head 
-
+solveRPN :: (Num a, Read a) => String -> a  
+solveRPN = head . foldl foldingFunction [] . words  
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+            foldingFunction (x:y:ys) "+" = (x + y):ys  
+            foldingFunction (x:y:ys) "-" = (y - x):ys  
+            foldingFunction xs numberString = read numberString:xs
